2ee3b543f2009b15cc4bf855157a25d545ceaead
nad-58 <nadernejad.ehsan@gmail.com>
2026-06-03 22:09:58 +0100

    Add neural network demo

---
+++ examples/neural_network_demo.py
@@ -0,0 +1,6 @@
+from ai_computational_approaches.neural_networks import architecture_summary, feedforward_demo
+
+
+if __name__ == "__main__":
+    print(feedforward_demo())
+    print(architecture_summary())
